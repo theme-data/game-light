16c05d3a891fb09380de3b343d26cddca4d2bc44
monteiroux <pd.data.user@gmail.com>
2026-04-22 20:31:14 -0300

    Sobe ajustess na alert bar

--- core.js
+++ core.js
@@ -695,8 +695,8 @@ if (CONFIG.bannerVitrine) {
 
   var textoAlertBar = CONFIG.alertBar || {};
 
-  if (alertBar.mensagem) {
-    $(`.barra-inicial`)
+  if (textoAlertBar.mensagem) {
+    $('.barra-inicial')
       .replaceWith(`
         <div class="alert-bar">
           <span>${textoAlertBar.mensagem}</span>
